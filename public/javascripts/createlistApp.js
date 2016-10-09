c2e712f188e8d35847328b56504c0ac46fc6b2ab
Vikram Gopali <vgopali@yodlee.com>
2016-10-09 13:17:21 +0530

    display dynamically users in different list based on catagories while creating list

--- public/javascripts/createlistApp.js
+++ public/javascripts/createlistApp.js
@@ -5,6 +5,7 @@ var app = angular.module('myApp1', []);
 
 app.controller('createlistctrllr',['$http', '$scope' , function ($http, $scope) {
     $scope.items = [];
+    $scope.catagoryArry = [];
     $scope.addItem = function () {
         console.log($scope.newItemName,$scope.newItemEmail)
         var fnameStr='initial',lnameStr='string';
@@ -16,11 +17,33 @@ app.controller('createlistctrllr',['$http', '$scope' , function ($http, $scope) 
             fnameStr = $scope.newItemName;
         }
         $scope.items.push({
-            //id: $scope.items.length + 1,
             fname: fnameStr,
             lname: lnameStr,
             email: $scope.newItemEmail,
             catagory:$scope.newItemCatagory
         });
+        var flag = false;
+        if($scope.catagoryArry.length == 0){
+            $scope.catagoryArry.push({'catagory' : $scope.newItemCatagory, 'usersArray':$scope.items});
+        }else{
+            /*for(var i=0;i<$scope.catagoryArry.length;i++){
+                if($scope.catagoryArry.catagory == $scope.newItemCatagory){
+                    flag = true;
+                    break;
+                }
+            }*/
+            angular.forEach($scope.catagoryArry, function(item){
+                if(item.catagory == $scope.newItemCatagory){
+                    flag = true;
+                    return;
+                }
+            })
+            if(!flag){
+                $scope.catagoryArry.push({'catagory' : $scope.newItemCatagory, 'usersArray':$scope.items});
+            }
+        }
+
+
+
     }
 }])
